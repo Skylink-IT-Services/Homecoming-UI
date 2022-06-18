2ae8a2127af1852ad74f651f633df6fb38cee89f
kinggatsheni <codedev95@outlook.com>
2022-06-18 05:26:42 +0200

    main

--- homecoming.webapp/ViewModel/Config.cs
+++ homecoming.webapp/ViewModel/Config.cs
@@ -2,7 +2,6 @@
 {
     public class Config
     {
-        //public static string BaseUrl { get { return "https://homecoming-cloud-api.azurewebsites.net/api/"; } }
-        public static string BaseUrl { get { return "https://localhost:5001/api/"; } }
+       public static string BaseUrl { get { return "https://homecoming-cloud-api.azurewebsites.net/api/"; } }
     }
 }
